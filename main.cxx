c0de318fd75bf40756ee1d529c352ca3f1280667
Tom Bennett <thomas.bennett5@gmail.com>
2020-07-31 16:16:21 +0100

    Added 2nd pokemon (ivysaur)

--- main.cxx
+++ main.cxx
@@ -4,7 +4,11 @@
 void foo()
 {
     pokemon bulbasaur(1, "Bulbasaur","Grass","Poison",318,45,49,49,65,65,45,1,0);
+    pokemon ivysaur (2,"Ivysaur","Grass","Poison",405,60,62,63,80,80,60,1,0);
+
     bulbasaur.printStats();
+    
+    ivysaur.printStats();
 }
 
 int main()
